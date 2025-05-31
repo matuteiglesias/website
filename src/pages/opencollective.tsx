08d621f09a4ef5df16673c288b2441a35cf30076
matuteiglesias <matuteiglesias@gmail.com>
2025-05-30 21:37:47 -0300

    update

--- src/pages/opencollective.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function Discord(): JSX.Element {
-	useEffect(() => {
-		window.location.href = 'https://opencollective.com/sapphiredev';
-	}, []);
-
-	return <></>;
-}
--- src/pages/patreon.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function Discord(): JSX.Element {
-	useEffect(() => {
-		window.location.href = 'https://www.patreon.com/sapphire_community';
-	}, []);
-
-	return <></>;
-}
